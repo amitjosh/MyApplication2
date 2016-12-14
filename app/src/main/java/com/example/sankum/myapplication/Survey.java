f5b0618dd946d97f6de5643761198027fc0c2d9d
amitjosh <amitjosh@umich.edu>
2016-12-14 17:01:19 -0500

    update project

--- app/src/main/java/com/example/sankum/myapplication/Survey.java
+++ app/src/main/java/com/example/sankum/myapplication/Survey.java
@@ -1,10 +1,15 @@
 package com.example.sankum.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Survey extends AppCompatActivity implements View.OnClickListener {
 
@@ -28,6 +33,7 @@ public class Survey extends AppCompatActivity implements View.OnClickListener {
         submit_button=(Button) findViewById(R.id.SUBMIT);
 
 
+        submit_button.setOnClickListener(this);
 
 
 
@@ -35,6 +41,21 @@ public class Survey extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     public void onClick(View view) {
+        Toast.makeText(this, "Loading the WOOF", Toast.LENGTH_SHORT).show();
+        //String name = editText.getText().toString();
+        //String emailAddress = editText2.getText().toString();
+        //String zipcode = editText3.getText().toString();
+        //RatingBar hometime = (RatingBar) ratingBar.getOnRatingBarChangeListener();
+        //RatingBar outdoor = (RatingBar) ratingBar2.getOnRatingBarChangeListener();
+        //Userprofile userprofile = new Userprofile(name,emailAddress, zipcode,hometime , outdoor);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Users");
+        // DatabaseReference newuser = myRef.push();
+        //myRef.setValue(userprofile);
+
+        Intent intent = new Intent(Survey.this, SplashActivity.class);
+        startActivity(intent);
 
     }
 }
